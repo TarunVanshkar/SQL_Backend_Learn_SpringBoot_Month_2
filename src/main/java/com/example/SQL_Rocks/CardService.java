b052fc15f9773cbde7effbbcbe2a982fc9b3a0ce
TarunVanshkar <tarunben33@gmail.com>
2023-02-09 22:42:44 +0530

    SQL_Backend_Integrate_Unidirectional

---
+++ src/main/java/com/example/SQL_Rocks/CardService.java
@@ -0,0 +1,4 @@
+package com.example.SQL_Rocks;
+
+public class CardService {
+}
---
+++ src/main/java/com/example/SQL_Rocks/CardStatus.java
@@ -0,0 +1,10 @@
+package com.example.SQL_Rocks;
+
+public enum CardStatus
+{
+    ACTIVATED,
+    DEACTIVATED,
+    EXPIRED,
+    MISSING,
+    BLOCKED
+}
